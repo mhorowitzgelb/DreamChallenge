0c7313ad4b4139dad6cda8e9c35e4dc6fb11b010
mhorowitzgelb <mhorowitzgelb@hotmail.com>
2015-04-29 21:13:19 -0500

    Created simple quick program to add month death classes to csv

---
+++ DreamChallengeIntellijProject/src/CreateMonthDeathFeatures.java
@@ -0,0 +1,59 @@
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import weka.core.converters.CSVSaver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by mhorowitzgelb on 4/29/2015.
+ */
+public class CreateMonthDeathFeatures {
+    public static void main(String[] args) throws IOException {
+        if(args.length != 2){
+            System.out.println("Please specify csv file as well as output csv file");
+            return;
+        }
+        CSVLoader loader = new CSVLoader();
+        loader.setSource(new File(args[0]));
+        Instances data = loader.getDataSet();
+
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("YES");
+        values.add("NO");
+
+        data.insertAttributeAt(new Attribute("Death12",values),5);
+        data.insertAttributeAt(new Attribute("Death18",values),6);
+        data.insertAttributeAt(new Attribute("Death24",values),7);
+
+        for(int i = 0; i < data.numInstances(); i ++){
+            Instance instance = data.instance(i);
+            if(instance.value(3) <= 365 && instance.stringValue(4).equals("YES")){
+                instance.setValue(5,"YES");
+            }
+            else{
+                instance.setValue(5,"NO");
+            }
+            if(instance.value(3) <= 546 && instance.stringValue(4).equals("YES")){
+                instance.setValue(6,"YES");
+            }
+            else{
+                instance.setValue(6,"NO");
+            }
+            if(instance.value(3) <= 730 && instance.stringValue(4).equals("YES")){
+                instance.setValue(7,"YES");
+            }
+            else{
+                instance.setValue(7,"NO");
+            }
+        }
+
+        CSVSaver saver = new CSVSaver();
+        saver.setInstances(data);
+        saver.setFile(new File(args[1]));
+        saver.writeBatch();
+    }
+}
