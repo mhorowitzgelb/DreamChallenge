cabb5a9b0eb8f4f7f5edfd750635b54e72f9a0f7
mhorowitzgelb <mhorowitzgelb@hotmail.com>
2015-05-05 22:03:17 -0500

    Created all training and leaderboard arff files in the correct format so they can be tested. All files have already been filtered for top 48 gain ratio values.

--- DreamChallengeIntellijProject/src/FuckingBayesFix.java
+++ DreamChallengeIntellijProject/src/FuckingBayesFix.java
@@ -1,8 +1,29 @@
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by mhorowitzgelb on 5/5/2015.
  */
 public class FuckingBayesFix {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws Exception {
+        CSVLoader loader = new CSVLoader();
+        loader.setSource(new File("C:\\Users\\mhorowitzgelb\\Documents\\DreamChallenge\\LeaderBoard\\ouput.csv"));
+        Instances instances = loader.getDataSet();
+        for(Instance i : instances){
+            if(i.stringValue(0).equals("1:YES")){
+                System.out.println(i.value(1));
+            }
+            else if(i.stringValue(0).equals("2:NO")){
+                System.out.println(1 - i.value(1));
+            }
+            else{
+                throw new Exception(i.stringValue(0));
+            }
+        }
     }
 }
